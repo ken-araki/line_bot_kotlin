4ec8b2083e89d9d768b6ff18350ec613c291f74c
ken-araki <k.takagaki.614@gmail.com>
2020-02-09 21:32:25 +0900

    fix bug @RestController

--- src/main/kotlin/com/linebot/controller/test/TobuyActionController.kt
+++ src/main/kotlin/com/linebot/controller/test/TobuyActionController.kt
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.Profile
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @RequestMapping(path = ["/test/action"])
 @Profile("local")
 class TobuyActionController(
