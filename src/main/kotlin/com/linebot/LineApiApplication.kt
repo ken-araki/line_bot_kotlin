dcdb2fed3d126f11f80285e3310dc1f60a621e08
ken-araki <k.takagaki.614@gmail.com>
2020-02-11 19:05:03 +0900

    refactor LineApiApplication

--- src/main/kotlin/com/linebot/LineApiApplication.kt
+++ src/main/kotlin/com/linebot/LineApiApplication.kt
@@ -1,14 +1,12 @@
 package com.linebot
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
+@SpringBootApplication
 @EnableScheduling
-@EnableAutoConfiguration
-@ComponentScan
-class LineApiApplication {
+open class LineApiApplication {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
