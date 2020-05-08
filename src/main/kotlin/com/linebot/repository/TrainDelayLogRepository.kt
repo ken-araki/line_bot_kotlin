5f892ca95415c63344cc5fe6570ff151290569d8
ken-araki <k.takagaki.614@gmail.com>
2020-05-09 00:04:55 +0900

    remove train delay log

--- src/main/kotlin/com/linebot/repository/TrainDelayLogRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.linebot.repository
-
-import com.linebot.entity.TrainDelayLog
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TrainDelayLogRepository : JpaRepository<TrainDelayLog, Long>
--- src/main/kotlin/com/linebot/service/notice/GetTrainDelayResourceService.kt
+++ src/main/kotlin/com/linebot/service/notice/GetTrainDelayResourceService.kt
@@ -1,39 +1,36 @@
 package com.linebot.service.notice
 
 import com.linebot.client.train.TrainDelayClient
-import com.linebot.service.log.LogService
 import com.linebot.service.message.PushMessageService
 import com.linebot.util.Utils
 import com.linecorp.bot.model.message.TextMessage
 import java.time.LocalDateTime
 import java.time.ZoneId
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class GetTrainDelayResourceService(
     private val pushMessageService: PushMessageService,
     private val trainDelayClient: TrainDelayClient,
-    private val noticeService: NoticeService,
-    private val logService: LogService
+    private val noticeService: NoticeService
 ) {
 
+    @Transactional
     fun execute() {
         val now = LocalDateTime.now(ZoneId.of("Asia/Tokyo"))
         val hour = now.hour
         val minute = now.minute
-
         val results = trainDelayClient.delay
-                .also { logService.insertTrainDelay(it) }
         val message = results.takeIf { it.isNotEmpty() }
-                ?.filterNot { it.name.isNullOrEmpty() }
-                ?.map { it.name }
-                ?.joinToString(Utils.LINE_SEPARATOR)
-                ?: "遅延している沿線はありません。"
-
+            ?.filterNot { it.name.isNullOrEmpty() }
+            ?.map { it.name }
+            ?.joinToString(Utils.LINE_SEPARATOR)
+            ?: "遅延している沿線はありません。"
         val userIds = noticeService.findTrainDelay()
-                .filter { it.hour == hour && it.minute == minute }
-                .mapNotNull { it.userId }
-                .toSet()
+            .filter { it.hour == hour && it.minute == minute }
+            .mapNotNull { it.userId }
+            .toSet()
 
         if (userIds.isNotEmpty()) {
             pushMessageService.multicast(userIds, listOf(TextMessage(message)))
