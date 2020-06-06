3f177303e68c8c26746ba020ea8c5e814ce80954
ken-araki <k.takagaki.614@gmail.com>
2020-06-07 01:02:04 +0900

    change requestParam to requestBody

--- src/main/kotlin/com/linebot/api/tobuy/TobuyController.kt
+++ src/main/kotlin/com/linebot/api/tobuy/TobuyController.kt
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -58,7 +59,7 @@ class TobuyController(
     fun add(
         @RequestHeader("token") token: String,
         @RequestHeader("nonce") nonce: String,
-        @RequestParam(name = "ids") ids: List<Int>
+        @RequestBody ids: List<Int>
     ): SuccessResponse {
         if (token == propertiesConfig.token && nonce.isEmpty() && ids.isEmpty()) {
             throw RuntimeException()
